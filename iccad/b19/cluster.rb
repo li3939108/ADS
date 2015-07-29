16258050bd7d9adfd461f97fa767735611342511
Chaofan Li <li3939108@gmail.com>
2015-07-28 21:32:24 -0500

    update

--- iccad/b19/cluster.rb
+++ iccad/b19/cluster.rb
@@ -1,21 +1,18 @@
-verilog_file_list = Dir['*.v']
-if verilog_file_list.length == 1 
-	verilog_file_name= verilog_file_list[0] 
-	cluster_file = File.new("gatesInClusters.txt", "r") 
-	verilog_file = File.new(verilog_file_name, "r" )
-	verilog = verilog_file.read
-	cluster_file.each do |line|
-		gate_out_cluster = line.split(" ") 
-		matched_line = verilog.match(/\w+[ ]+\w+[ ]*\(.+\(#{gate_out_cluster[0]}\)[ ]*\);/)
-		if(matched_line == nil)
-			raise("no such output wire")
-		else
-			puts matched_line[0] 
-		end
-	end
-	cluster_file.close
-elsif verilog_file_list.length  == 0
-	raise("no verilog file in this directory") 
-else
-	raise("more than one verilog file in this directory") 
+gate_out_file = File.new("gates.txt")
+cluster_file = File.new("gatesInClusters.txt", "r") 
+out2gate = {}
+gate2cluster = {}
+gate_out_file.each do |line|
+	gate_out_pair = line.split ( ' ')
+	out2gate[gate_out_pair[1] ] = gate_out_pair[0] if gate_out_pair.length == 2
 end
+gate_out_file.close
+cluster_file.each do |line|
+	out_cluster = line.split(" ") 
+	if out_cluster.length == 2 
+		gate2cluster[ out2gate[ out_cluster[0] ] ] = out_cluster[1] 
+		puts "#{out2gate[out_cluster[0] ]} #{out_cluster[1]}"
+	end
+end
+cluster_file.close
+
