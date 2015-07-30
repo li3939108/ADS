55b064b3012853f99b50486dc14b30d5bc947e93
Chaofan Li <li3939108@gmail.com>
2015-07-30 17:48:44 -0500

    update

--- iccad/timing.rb
+++ iccad/timing.rb
@@ -3,6 +3,7 @@ INITIAL = 0
 HEADER = 1
 TIMING_PATH_HEADER = 2
 TIMING_PATH = 3
+TIMING_PATH_MULTILINE = 4
 class Path
 	def initialize(startpoint = nil, endpoint = nil, at = 0)
 		@startpoint = startpoint
@@ -23,14 +24,14 @@ timing_file.each do | line |
 	if state == INITIAL
 		if line.include?("***************************")
 			state = HEADER 
-			puts( " -> HEADER")
+#			puts( " -> HEADER")
 		end
 	elsif state == HEADER 
 		if line[0..7] == "Design :"
 			design_name = line[9..-2] 
 		elsif line.include?("***************************") 
 			state = TIMING_PATH_HEADER 
-			puts( " -> TIMING_PATH_HEADER")
+#			puts( " -> TIMING_PATH_HEADER")
 		end
 	elsif state == TIMING_PATH_HEADER 
 		line_seg = line.split(/[\s:]+/) 
@@ -42,10 +43,22 @@ timing_file.each do | line |
 			path.set_endpoint ( line_seg[2] ) 
 		elsif line_seg[1].include?("------------------------") 
 			state = TIMING_PATH
-			puts( " -> TIMING_PATH")
+#			puts( " -> TIMING_PATH")
 		end
 	elsif state == TIMING_PATH
+		line_seg = line.split(/[\s()\/]+/)
+			
+		if line_seg[-1] == "r" or line_seg[-1] == "f"
+			if line_seg [-3] != "0.00"
+				if line_seg.length == 4			
+					path.add_gates(line_seg[1], line_seg[2], line_seg[3] ) 
+					state = TIMING_PATH_MULTILINE
+				elsif line_seg.length == 6
+					path.add_gates(line_seg[1], nil , line_seg[2] ) 
+				end
+			end
+		elsif line_seg[1] == "data" and line_seg[2] == "arrival" and line_seg[3] == "time"
+		end
 		#critical_paths.push(path) 
 	end
 end
-
