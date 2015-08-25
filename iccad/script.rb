e5af4e5e34e77d0cd6fe2ba1546838b837e82308
Chaofan Li <li3939108@gmail.com>
2015-08-24 20:19:49 -0500

    update

--- iccad/script.rb
+++ iccad/script.rb
@@ -7,9 +7,24 @@ ap = mat_gen(ckt.critical_paths, ckt.clusters, 0.25)
 cost_gen(ap, ckt.clusters) 
 
 # compare 
-(0..100).each do |i|
+sum1 = 0
+sum2 = 0
+count = 0
+(0..99).each do |i|
 	sp = ckt.random_paths(2)
 	r = simu_knob(ap, sp.to_set, ckt.clusters)
 	rr = naive_simu_knob(ap, sp.to_set, ckt.clusters)
-	print leakage_diff(r, ckt.clusters, lib, lib2) , leakage_diff(rr, ckt.clusters, lib, lib2), "\n"
+	ld1 = leakage_diff(r, ckt.clusters, lib, lib2)
+	sum1 += ld1
+	ld2 = leakage_diff(rr, ckt.clusters, lib, lib2)
+	sum2 += ld2
+
+	if ld2 > ld1 
+		count += 1
+	end
+	
+	print "logic: ",  ld1 , " naive: ", ld2, " diff:", ld2 - ld1,"\n"
 end
+print "proposed method: ", sum1 / 99, "\n"
+print "naive method: ", sum2 / 99, "\n"
+print "count: ", count, "\n"
