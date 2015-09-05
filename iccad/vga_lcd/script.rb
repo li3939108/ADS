f143972ccbba7dbe99be32631e1b845af8720ede
Chaofan Li <li3939108@gmail.com>
2015-09-04 19:44:03 -0500

    update

--- iccad/vga_lcd/script.rb
+++ iccad/vga_lcd/script.rb
@@ -7,17 +7,17 @@ ap = mat_gen(ckt.critical_paths, ckt.clusters, 0.25)
 cost_gen(ap, ckt.clusters) 
 
 # compare 
-#sum1 = 0
-#sum2 = 0
+#sum_logic = 0
+#sum_naive = 0
 #count = 0
 #(0..99).each do |i|
 #	sp = ckt.random_paths(2)
 #	r = simu_knob(ap, sp.to_set, ckt.clusters)
 #	rr = naive_simu_knob(ap, sp.to_set, ckt.clusters)
 #	ld1 = leakage_diff(r, ckt.clusters, lib, lib2)
-#	sum1 += ld1
+#	sum_logic += ld1
 #	ld2 = leakage_diff(rr, ckt.clusters, lib, lib2)
-#	sum2 += ld2
+#	sum_naive += ld2
 #
 #	if ld2 > ld1 
 #		count += 1
@@ -25,37 +25,51 @@ cost_gen(ap, ckt.clusters)
 #	
 #	print "logic: ",  ld1 , " naive: ", ld2, " diff:", ld2 - ld1,"\n"
 #end
-#print "proposed method: ", sum1 / 99, "\n"
-#print "naive method: ", sum2 / 99, "\n"
+#print "proposed method: ", sum_logic / 99, "\n"
+#print "naive method: ", sum_naive / 99, "\n"
 #print "count: ", count, "\n"
 
-sum1 = 0
-sum2 = 0
-true_count1 = 0
-true_count2 = 0
+sum_logic = 0
+sum_naive = 0
+true_count_logic = 0
+true_count_naive = 0
+true_count_all_low = 0
+true_count_all_high = 0
 times = 5000
+rat = 2.1
+all_cluster_paths = ckt.clusters.to_id.map{|id| [id,[]] }
 
 (1..times).each do |i|
 	ckt.update_variation
 	on = ckt.simu_sensor(2)
 	r = simu_knob(ap, on.to_set, ckt.clusters)
 	rr = naive_simu_knob(ap, on.to_set, ckt.clusters)
-	ld1 = leakage_diff(r, ckt.clusters, lib, lib2)
-	sum1 += ld1
-	ld2 = leakage_diff(rr, ckt.clusters, lib, lib2)
-	sum2 += ld2
-	true_count1 += 1 if ckt.check_timing(2.1, r)
-	true_count2 += 1 if ckt.check_timing(2.1, rr)
+	ld_logic = leakage_diff(r, ckt.clusters, lib, lib2)
+	sum_logic += ld_logic
+	ld_naive = leakage_diff(rr, ckt.clusters, lib, lib2)
+	sum_naive += ld_naive
+	true_count_logic += 1 if ckt.check_timing(rat, r)
+	true_count_naive += 1 if ckt.check_timing(rat, rr)
+	true_count_all_low += 1 if ckt.check_timing(rat, [] ) 
+	true_count_all_high += 1 if ckt.check_timing(rat, all_cluster_paths  )
 end
 
 
 print "proposed method \n"
 print "------------\n"
-print "leakage increase: ", sum1 / (0.0 + times), "\n"
-print "timing yield: ", true_count1 / (0.0 + times), "\n", "\n"
+print "leakage increase: ", sum_logic / (0.0 + times), "\n"
+print "timing yield: ", true_count_logic / (0.0 + times), "\n", "\n"
 
 print "naive method \n"
 print "------------\n"
-print "leakage increase: ", sum2 / (0.0 + times), "\n"
-print "timing yield: ", true_count2 / (0.0 + times), "\n"
+print "leakage increase: ", sum_naive / (0.0 + times), "\n"
+print "timing yield: ", true_count_naive / (0.0 + times), "\n"
 
+print "all low \n"
+print "-------------\n"
+print "timing yield: ", true_count_all_low / (0.0 + times ), "\n", "\n"
+
+print "all high \n"
+print "-------------\n"
+print "leakage increase: ", leakage_diff(all_cluster_paths, ckt.clusters, lib, lib2), "\n"
+print "timing yield: ", true_count_all_high / (0.0 + times) , "\n", "\n"
