9b45c483ec003bd5b2c22cfdfc7900a3de9486f9
me@hera.ece <li3939108@gmail.com>
2015-09-20 00:47:22 -0500

    report

--- iccad/mgc_matrix_mult/script.tcl
+++ iccad/mgc_matrix_mult/script.tcl
@@ -1,7 +1,7 @@
 set search_path {.}
-#set link_path {*  NangateOpenCellLibrary_typical_ccs.lib}
+set link_path {*  NangateOpenCellLibrary_typical_ccs.lib}
 #set link_path {* NangateOpenCellLibrary_fast_ccs.lib}
-set link_path {* NangateOpenCellLibrary_low_temp_ccs.lib }
+#set link_path {* NangateOpenCellLibrary_low_temp_ccs.lib }
 read_verilog mgc_matrix_mult.v
 current_design matrix_mult
 link_design
