9d76035f57b64a79bb8fec0c2862f70df58882ba
Chaofan Li <li3939108@gmail.com>
2015-09-29 21:38:01 -0500

    unified script

--- iccad/script.rb
+++ iccad/script.rb
@@ -1,33 +1,10 @@
 require './timing.rb'
 lib = Library.new('NangateOpenCellLibrary_typical_ccs.lib')
 lib2 = Library.new('NangateOpenCellLibrary_fast_ccs.lib')
-ckt = Circuit.new('gates.txt', lib)
+ckt = Circuit.new('gates.txt', lib, 10, 0.1, 'br')
 #control_ckt = Circuit.new('gates.control', lib)
-ap = mat_gen(ckt.critical_paths, ckt.clusters, 0.25)
+ap = mat_gen(ckt.critical_paths, ckt.clusters, 0.2)
 cost_gen(ap, ckt.clusters) 
-
-# compare 
-#sum1 = 0
-#sum2 = 0
-#count = 0
-#(0..99).each do |i|
-#	sp = ckt.random_paths(2)
-#	r = simu_knob(ap, sp.to_set, ckt.clusters)
-#	rr = naive_simu_knob(ap, sp.to_set, ckt.clusters)
-#	ld1 = leakage_diff(r, ckt.clusters, lib, lib2)
-#	sum1 += ld1
-#	ld2 = leakage_diff(rr, ckt.clusters, lib, lib2)
-#	sum2 += ld2
-#
-#	if ld2 > ld1 
-#		count += 1
-#	end
-#	
-#	print "logic: ",  ld1 , " naive: ", ld2, " diff:", ld2 - ld1,"\n"
-#end
-#print "proposed method: ", sum1 / 99, "\n"
-#print "naive method: ", sum2 / 99, "\n"
-#print "count: ", count, "\n"
 
 sum_logic = 0
 sum_naive = 0
@@ -35,33 +12,48 @@ true_count_logic = 0
 true_count_naive = 0
 true_count_all_low = 0
 true_count_all_high = 0
-times = 5000
-rat = 6.45
+on_count = 0
+on_2_count = 0
+times = ARGV[1].to_i
+rat = ARGV[0].to_f
 all_cluster_paths = ckt.clusters.to_id.map{|id| [id,[]] }
 
 (1..times).each do |i|
 	ckt.update_variation
-	on = ckt.simu_sensor(6.45)
-	r = simu_knob(ap, on.to_set, ckt.clusters)
-	rr = naive_simu_knob(ap, on.to_set, ckt.clusters)
-	ld_logic = leakage_diff(r, ckt.clusters, lib, lib2)
-	sum_logic += ld_logic
-	ld_naive = leakage_diff(rr, ckt.clusters, lib, lib2)
-	sum_naive += ld_naive
-	true_count_logic += 1 if ckt.check_timing(rat, r)
-	true_count_naive += 1 if ckt.check_timing(rat, rr)
-	true_count_all_low += 1 if ckt.check_timing(rat, [] ) 
-	true_count_all_high += 1 if ckt.check_timing(rat, all_cluster_paths  )
+	on = ckt.simu_sensor(rat)
+	if on.length == 0
+		if ckt.check_timing(rat, [] ) 
+			true_count_all_low += 1 
+			true_count_logic += 1 
+			true_count_naive += 1 
+		end
+		true_count_all_high += 1 #if ckt.check_timing(rat, all_cluster_paths  )
+	else
+		on_2_count += 1 if on.length > 1
+		on_count += 1
+		r = simu_knob(ap, on.to_set, ckt.clusters)
+		rr = naive_simu_knob(ap, on.to_set, ckt.clusters)
+		ld_logic = leakage_diff(r, ckt.clusters, lib, lib2)
+		sum_logic += ld_logic
+		ld_naive = leakage_diff(rr, ckt.clusters, lib, lib2)
+		sum_naive += ld_naive
+		true_count_logic += 1 if ckt.check_timing(rat, r)
+		true_count_naive += 1 if ckt.check_timing(rat, rr)
+		true_count_all_low += 1 if ckt.check_timing(rat, [] ) 
+		true_count_all_high += 1 #if ckt.check_timing(rat, all_cluster_paths  )
+	end
 end
-
+print "2 sensors on count:", on_2_count, "\n"
+print "on count:", on_count, "\n"
+print "total count:", times, "\n"
 print "proposed method \n"
 print "------------\n"
-print "leakage increase: ", sum_logic / (0.0 + times), "\n"
+print "leakage increase: ", sum_logic / (0.0 + times), ' ', sum_logic / (0.0 + on_count) , "\n"
 print "timing yield: ", true_count_logic / (0.0 + times), "\n", "\n"
 
 print "naive method \n"
 print "------------\n"
-print "leakage increase: ", sum_naive / (0.0 + times), "\n"
+print "leakage increase: ", sum_naive / (0.0 + times), ' ', sum_naive / (0.0 + on_count),"\n"
 print "timing yield: ", true_count_naive / (0.0 + times), "\n", "\n"
 
 print "all low \n"
